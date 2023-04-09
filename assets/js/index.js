4fd6e5a7bd06bdff5bf60d3f06c5d0ba5f84ab54
Sara Wendland <info@rosebudsara.com>
2023-04-08 19:22:56 -0500

    added current weather div

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ var searchBtn = document.getElementById('searchButton');
 var city;
 var temp;
 var input = document.getElementById('userInput')
+const weatherResult = document.getElementById('weatherResult')
 
 
 
@@ -12,33 +13,24 @@ var input = document.getElementById('userInput')
 searchBtn.addEventListener('click', e => {
     e.preventDefault()
     console.log('search button clicked')
-    // addElement();
-
-
-
+    
 
     let inputVal = input.value
 
-    
-    
-
+        
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=imperial`
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
        console.log(data)
+
+    
       
         
     addElement();
 
-
-
-
-
-    })
-
-        
+    })       
             
 
 })
@@ -49,13 +41,13 @@ searchBtn.addEventListener('click', e => {
 
 function addElement() {
   // create a new div element
-  const newDiv = document.createElement("div");
+  const newDiv = document.createElement("ul");
 
   // and give it some content
   const newContent = document.createTextNode("Hi there and greetings!");
 
   // add the text node to the newly created div
-  newDiv.appendChild(newContent);
+  newDiv.appendChild(weatherResult);
 
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("div1");
