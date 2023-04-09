cb6f85ae86e760e73191785b5f1e7fc9eeaae938
Sara Wendland <info@rosebudsara.com>
2023-04-09 10:58:28 -0500

    deleted unnecessary comment

--- assets/js/index.js
+++ assets/js/index.js
@@ -51,9 +51,7 @@ searchBtn.addEventListener('click', e => {
       // add the text node to the newly created div
       weatherResult2.appendChild(currentDayContent);
     
-      // add the newly created element and its content into the DOM
-    //   const currentDiv = document.getElementById("div1");
-    //   document.body.insertBefore(newDiv, currentDiv);
+    
     }
 
     })       
