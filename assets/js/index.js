56189d91d8e2ed1b2f329356b93980ee2d3f0381
Sara Wendland <info@rosebudsara.com>
2023-04-08 19:43:35 -0500

    current weather div created

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,7 +18,7 @@ searchBtn.addEventListener('click', e => {
     let inputVal = input.value
 
         
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=imperial`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`
 
     fetch(url)
     .then(response => response.json())
@@ -30,6 +30,24 @@ searchBtn.addEventListener('click', e => {
         
     addElement();
 
+    function addElement() {
+
+        var cityName = (data.name)
+        var cityWeather = (data.main.temp)
+      // create a new div element
+      const newDiv = document.createElement("ul");
+    
+      // and give it some content
+      const newContent = document.createTextNode("The Current Weather in " + cityName + " is " + cityWeather + "! ");
+    
+      // add the text node to the newly created div
+      weatherResult.appendChild(newContent);
+    
+      // add the newly created element and its content into the DOM
+      const currentDiv = document.getElementById("div1");
+      document.body.insertBefore(newDiv, currentDiv);
+    }
+
     })       
             
 
@@ -40,14 +58,17 @@ searchBtn.addEventListener('click', e => {
 
 
 function addElement() {
+
+    var cityName = (data.name)
+    var cityWeather = (data.main.temp)
   // create a new div element
   const newDiv = document.createElement("ul");
 
   // and give it some content
-  const newContent = document.createTextNode("Hi there and greetings!");
+  const newContent = document.createTextNode("The Current Weather in" + cityName + "is" + cityWeather + "!");
 
   // add the text node to the newly created div
-  newDiv.appendChild(weatherResult);
+  weatherResult.appendChild(newContent);
 
   // add the newly created element and its content into the DOM
   const currentDiv = document.getElementById("div1");
