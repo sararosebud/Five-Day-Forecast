7452b4c8a870e5a27f7c274d54405f1c06a15091
Sara Wendland <info@rosebudsara.com>
2023-04-08 19:10:03 -0500

    resolved authentication error, fetched api

---
+++ assets/js/index.js
@@ -0,0 +1,63 @@
+const apiKey = "976a68c72902bf9ad934e239ce2e5e99"
+var searchBtn = document.getElementById('searchButton');
+var city;
+var temp;
+var input = document.getElementById('userInput')
+
+
+
+
+
+
+searchBtn.addEventListener('click', e => {
+    e.preventDefault()
+    console.log('search button clicked')
+    // addElement();
+
+
+
+
+    let inputVal = input.value
+
+    
+    
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal}&appid=${apiKey}&units=imperial`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+       console.log(data)
+      
+        
+    addElement();
+
+
+
+
+
+    })
+
+        
+            
+
+})
+
+
+
+
+
+function addElement() {
+  // create a new div element
+  const newDiv = document.createElement("div");
+
+  // and give it some content
+  const newContent = document.createTextNode("Hi there and greetings!");
+
+  // add the text node to the newly created div
+  newDiv.appendChild(newContent);
+
+  // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
