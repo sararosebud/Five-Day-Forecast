e9c12ef3fdd0f13415600d320b870b30f1d05519
Sara Wendland <info@rosebudsara.com>
2023-04-18 13:03:20 -0500

    added local storage and buttons, cleared repeat

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,8 @@ var city;
 var temp;
 var input = document.getElementById('userInput')
 const weatherResult = document.getElementById('weatherResult')
+var cityList = document.getElementById("savedCities")
+
 
 
 
@@ -14,54 +16,127 @@ $('').text(today.format('MMM D, YYYY'));
 function addElement(data) {        
 
   var cityName = (data.name)
-  // var currentDate = (data.
-  var cityWeather = (data.main.temp)
-// create a new div element
+  var dayOneTemp = (data.main.temp)
+  var dayOneHumidity = data.main.humidity
+  var dayOnePressure = data.main.pressure
+ 
+
+// current day container with information that populates with the day's weather
 const currentDay = today.format('MM/DD/YYYY');
 
+console.log(data)
+//Content that goes into the container
+const currentDayTitle = document.createElement('h5')
+currentDayTitle.classList.add('card-body')
+currentDayTitle.textContent = "The Weather In" + ' ' + cityName
+// body of card to hold all of the weather information
+const cdCardBody = document.createElement('div');
+cdCardBody.classList.add("card-body")
 
-// and give it some content
-const currentDayContent = document.createTextNode( "The Weather In: " + cityName + " (" + currentDay + ")" + "   " + cityWeather + " ");
+// today's date
+const currentDate = document.createElement('p');
+currentDate.textContent = currentDay;
+// today's temperature
+const currentTemp = document.createElement('p');
+currentTemp.classList.add("card-body")
+currentTemp.textContent = "Temperature:" + dayOneTemp + "°" + "F"
+// today's humidity
+const currentHumidity = document.createElement('p');
+currentHumidity.classList.add('card-body');
+currentHumidity.textContent = "Humidity: " + " " + dayOneHumidity + "%"
+// today's pressure
+const currentPressure = document.createElement('p');
+currentPressure.classList.add('card-body');
+currentPressure.textContent = "Pressure: " + " " + dayOnePressure
 
-// add the text node to the newly created div
-weatherResult2.appendChild(currentDayContent);
+// appending all of the created elements to the card body and weather container
+weatherResult.appendChild(cdCardBody);
+cdCardBody.appendChild(currentDayTitle);
+cdCardBody.appendChild(currentDate);
+cdCardBody.appendChild(currentTemp);
+cdCardBody.appendChild(currentHumidity);
+cdCardBody.appendChild(currentPressure);
 
 
 }
 
 
+// function that saves the searched cities into local storage, populates them to a button, and repopulates the forecast for that city
+
+function saveLastCity(cityName) {
+  var storedCity = {
+    city: cityName
+  };
+  localStorage.setItem("savedCity", JSON.stringify(storedCity));
+}
+
+function renderLastCity() {
+  var lastCity = JSON.parse(localStorage.getItem("savedCity"));
+  if (lastCity !== null) {
+    var cityName = lastCity.city;
+    var cityButton = document.createElement("button");
+    cityButton.textContent = cityName;
+    cityButton.addEventListener("click", function() {
+      var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          weatherResult.innerHTML = "";
+          addElement(data);
+          let lat = data.coord.lat;
+          let lon = data.coord.lon;
+          getForecast(lat, lon);
+        });
+        weatherResult.innerHTML = "";
+ document.getElementById('forecast').innerHTML = "";
+
+    });
+    cityList.appendChild(cityButton);
+  }
+}
+
+
+// search button to find city and populate weather forecast
+
 searchBtn.addEventListener('click', e => {
     e.preventDefault()
-    console.log('search button clicked')
+    
     
 
     let inputVal = input.value
-
-        
+   
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`
+
+    
+ // Clear the contents of weatherResult and forecast containers
+ weatherResult.innerHTML = "";
+ document.getElementById('forecast').innerHTML = "";
+
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
-       
-       console.log(data.coord.lat, data.coord.lon)
-       var lat = data.coord.lat;
-       var lon = data.coord.lon;
-
-
     
-      
+
+       let lat = data.coord.lat;
+       let lon = data.coord.lon;
+
         
     addElement(data);
     getForecast(lat, lon);
-  
 
- 
+    saveLastCity(inputVal);
+    
+    renderLastCity();
 
+    
+    
     })       
             
 
 })
+
+// call/fetch a five day forecast from openweathermap api
 
 function getForecast(lat, lon) {
 
@@ -93,18 +168,16 @@ function renderForecast(data) {
     let cardBody = document.createElement('div')
     cardBody.classList.add("card-body")
     let cardTitle = document.createElement('h5')
+    let cardImage = document.createElement('img')
+    cardImage.classList.add('card-img-top')
 
     let fiveDayDate = data.list[i].dt_txt
     let formattedDate = dayjs(fiveDayDate).format('ddd MMM D, YYYY');
-cardTitle.textContent = formattedDate;
-   
-    
-  
 
-
+    cardTitle.textContent = formattedDate;     
     cardTitle.classList.add('card-title')
 
-    // cardTitle.textContent = fiveDayDate
+  
 
 
     let humidityEl = document.createElement('p');
@@ -117,17 +190,19 @@ cardTitle.textContent = formattedDate;
     pressureEl.textContent = "Pressure:" + " " + data.list[i].main.pressure
 
     let iconEl = document.createElement('img');
-    iconEl.innerHTML = data.list[i].weather.icon
+    iconEl.textContent = data.list[i].weather.icon
+    console.log(iconEl)
 
    
 
 
     cardBody.append(cardTitle)
+    cardImage.append(iconEl);
 
     cardBody.append(temperatureEl);
     cardBody.append(humidityEl);
     cardBody.append(pressureEl);
-    cardBody.append(iconEl)
+    
     
     
     forecastCard.append(cardBody)
@@ -137,26 +212,4 @@ cardTitle.textContent = formattedDate;
  
 }
 
-
-
-// // Wait for document to load
-// $(document).ready(() => {
-//   $('.input-group').on('submit', () => {
-
-//       // prevents default behaviour
-//       // Prevents event propagation
-//       return false;
-//   });
-// });
-
-// $('.input-group').keypress((e) => {
-
-//   // Enter key corresponds to number 13
-//   if (e.which === 13) {
-//       $('.input-group').submit();
-//       console.log('form submitted');
-      
-     
-//   }
-// })
 
